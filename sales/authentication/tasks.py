5d413750478abdce30587fc75f978e4db3c0af58
Troggdorrr <vaxnini12345@gmail.com>
2023-04-27 13:39:03 +0300

    change email sending

--- sales/authentication/tasks.py
+++ sales/authentication/tasks.py
@@ -1,4 +1,5 @@
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+
 from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.template.loader import render_to_string
@@ -11,32 +12,30 @@ from sales.celery import app
 @app.task
 def send_success_signup_email(user_pk: int) -> None:
     user = get_user_model().objects.get(pk=user_pk)
-    message = render_to_string(
+    message_text = render_to_string(
         "authentication/new_user_mail.html",
         {"username": user.username},
     )
-    send_mail(
+    message = EmailMessage(
         "Успешная регистрация!",
-        f"{user.username}, вы успешно зарегистрировались на сайте SaleHunter",
-        settings.EMAIL_HOST_USER,
-        [user.email],
-        fail_silently=False,
-        html_message=message
+        message_text,
+        to=[user.email],
     )
+    message.content_subtype = "html"
+    message.send()
 
 
 @app.task
 def send_reset_password_email(email: str, reset_uuid: str) -> None:
     host = environ.get("HOST")
-    message = render_to_string(
+    message_text = render_to_string(
         "authentication/reset_password_mail.html",
         {"reset_link": f"{host}/reset-password?uuid={reset_uuid}"},
     )
-    send_mail(
-        subject="Восстановление пароля",
-        message=f"Ссылка для восстановления пароля: {host}/reset-password?uuid={reset_uuid}",
-        html_message=message,
-        from_email=settings.EMAIL_HOST_USER,
-        recipient_list=[email],
-        fail_silently=False,
+    message = EmailMessage(
+        "Восстановление пароля",
+        message_text,
+        to=[email],
     )
+    message.content_subtype = "html"
+    message.send()
